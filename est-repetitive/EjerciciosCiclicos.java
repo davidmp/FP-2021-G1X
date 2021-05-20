7130face040e688e284fd6704a6771240bc9c8ec
davidmp <mamanipari@gmail.com>
2021-05-20 11:48:47 -0500

    4.20

--- est-repetitive/EjerciciosCiclicos.java
+++ est-repetitive/EjerciciosCiclicos.java
@@ -35,12 +35,47 @@ public class EjerciciosCiclicos {
         System.out.println("La funcion exponencial de e^x es:"+fx);
     }
 
+
+    public static void hallarSenoX() {
+        //Declarar Variables
+        int senx, lfunSenx;        
+        //Datos de entrada
+        System.out.println("Ingrese un numero X:");
+        senx=teclado.nextInt();
+        System.out.println("Ingrese el limite de la funcion x:");
+        lfunSenx=teclado.nextInt();     
+        double fresulSenX=0;
+        //proceso        
+        int contSigno=0;
+        if(senx!=0){
+            for (int i = 1; i < lfunSenx; i+=2) {                
+                //fx+=((Math.pow(x, i))/hallarFactorial(i));
+                System.out.println("i="+i);
+                if (contSigno%2==0) {
+                    System.out.println("signo +");
+                    fresulSenX=fresulSenX+((Math.pow(senx, i))/hallarFactorial(i));
+                } else {
+                    fresulSenX=fresulSenX-((Math.pow(senx, i))/hallarFactorial(i));
+                    System.out.println("signo -");
+                }
+                //System.out.println(fresulSenX);
+                contSigno++;
+            }
+        }         
+        //Datos de de salida
+        System.out.println("Sen x es:"+fresulSenX);
+        
+    }
+
+
     
     public static void main(String[] args) {
         /*System.out.println("Ingrese un numero para calcular su factorial:");
         int numerox=teclado.nextInt();
         System.out.println("Factoria de "+numerox+" es:"+ hallarFactorial(numerox));*/
-        calcularFuncionExponecial();
+        //calcularFuncionExponecial();
+
+        hallarSenoX();
     }
 
 
