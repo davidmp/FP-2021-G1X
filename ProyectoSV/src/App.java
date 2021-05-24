db835c20f3432a327a9c566cd012e015f27315dc
davidmp <mamanipari@gmail.com>
2021-05-24 15:20:41 -0500

    Ejemplo Subprograma

---
+++ ProyectoSV/src/App.java
@@ -0,0 +1,11 @@
+import pe.edu.upeu.SubProgramas;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        SubProgramas objSP=new SubProgramas();
+        objSP.calcularFuncionExponecial();
+
+    }
+}
