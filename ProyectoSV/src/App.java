a095cab5b8269bb3746770d277ca2a8b9fdb263a
davidmp <mamanipari@gmail.com>
2021-05-31 15:20:35 -0500

    Ejmplo recursivo Fibonaci

--- ProyectoSV/src/App.java
+++ ProyectoSV/src/App.java
@@ -1,12 +1,21 @@
 import pe.edu.upeu.SubProgramas;
+import pe.edu.upeu.recur.EjemplosRecursivos;
+import pe.edu.upeu.util.TecladoRead;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
         SubProgramas objSP=new SubProgramas();
+        EjemplosRecursivos objER=new EjemplosRecursivos();
+        TecladoRead tr=new TecladoRead();
         //objSP.calcularFuncionExponecial();
-    System.out.println("Factorial: "+objSP.factorialBig(1000)); 
+        int numero=tr.read(0, "Indroducir un numero:");
+        /*System.out.println("Factorial iterativo: "+objER.factorialBig(numero)); 
+        System.out.println("Factorial Recursivo: "+objER.factorialBigRecur(numero)); */
+
+        System.out.println("Fibonaci iterativo: "+objER.fibonaci(numero)); 
+        System.out.println("Fibonaci recursivo: "+objER.fibonaciRecur(numero)); 
 
     }
 }
