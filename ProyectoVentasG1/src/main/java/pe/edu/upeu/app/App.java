9e425c810effe5177585c5d8fa60c5f4ec1a1cbd
davidmp <mamanipari@gmail.com>
2021-06-28 15:46:08 -0500

    Ejemplo Crear categoria

--- ProyectoVentasG1/src/main/java/pe/edu/upeu/app/App.java
+++ ProyectoVentasG1/src/main/java/pe/edu/upeu/app/App.java
@@ -1,5 +1,6 @@
 package pe.edu.upeu.app;
 
+import pe.edu.upeu.dao.CategoriaDAO;
 import pe.edu.upeu.gui.MainGUI;
 
 /**
@@ -9,6 +10,11 @@ import pe.edu.upeu.gui.MainGUI;
 public class App{
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
-        new MainGUI();
+        CategoriaDAO daoCat=new CategoriaDAO();
+        
+        daoCat.registrarCategoria();
+        daoCat.reporteCategoria();
+
+        //new MainGUI();
     }
 }
