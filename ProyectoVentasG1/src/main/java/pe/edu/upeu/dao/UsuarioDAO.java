00e7abd74b91736d3a83322ecb81c20085b3cae6
davidmp <mamanipari@gmail.com>
2021-07-01 10:47:46 -0500

    Crear usuario

--- ProyectoVentasG1/src/main/java/pe/edu/upeu/dao/UsuarioDAO.java
+++ ProyectoVentasG1/src/main/java/pe/edu/upeu/dao/UsuarioDAO.java
@@ -1,5 +1,7 @@
 package pe.edu.upeu.dao;
 
+import java.io.Console;
+
 import pe.edu.upeu.data.AppCrud;
 import pe.edu.upeu.modelo.UsuarioTO;
 import pe.edu.upeu.util.LeerArchivo;
@@ -11,5 +13,20 @@ public class UsuarioDAO extends AppCrud{
     UsuarioTO usuTO;
     
     TecladoRead  tre=new TecladoRead();
-    UtilsX ut=new UtilsX();    
+    UtilsX ut=new UtilsX(); 
+    
+    public void registrarUsuario() {
+        usuTO=new UsuarioTO();
+        String usuario=tre.read("", "Ingrese un Usuario:");
+        usuTO.setUsuario(usuario);
+        usuTO.setPerfil(tre.read("", "Ingrese el perfil (ADMIN, VENDEDOR, CAJERO):"));
+        lar=new LeerArchivo("Usuario.txt");
+        usuTO.setIdUsuario(generarId(lar, 0, "U", 1));
+        Console cons=System.console();
+        System.out.println("Ingrese su clave:");
+        char[] clave=cons.readPassword();
+        usuTO.setClave(String.valueOf(clave));
+        lar=new LeerArchivo("Usuario.txt");
+        agregarContenido(lar, usuTO);
+    }
 }
