674d50c599463375b3e5b436f93df5a38a14f2ff
davidmp <mamanipari@gmail.com>
2021-07-05 13:49:11 -0500

    Crear Producto

--- ProyectoVentasG1/src/main/java/pe/edu/upeu/dao/ProductoDAO.java
+++ ProyectoVentasG1/src/main/java/pe/edu/upeu/dao/ProductoDAO.java
@@ -21,7 +21,14 @@ public class ProductoDAO extends AppCrud{
         proTO=new ProductoTO();
         proTO.setIdCateg(tre.read("", "Seleccion el ID categoria:"));        
         lar=new LeerArchivo("Producto.txt");
-        
+        proTO.setIdProducto(generarId(lar, 0, "P", 1));
+        proTO.setNombre(tre.read("", "Ingrese el nombre del Producto:"));
+        proTO.setUnidadMed(tre.read("","Ingrese Unidad de Medidad:"));
+        proTO.setPreUnitario(tre.read(0.0, "Ingrese el Precio Unitario:"));
+        proTO.setPorcentUtil(tre.read(0.0, "Ingrese el % de Utilidad:"));
+        proTO.setStock(tre.read(0.0, "Ingrese el Stock:"));
+        lar=new LeerArchivo("Producto.txt");
+        agregarContenido(lar, proTO);
     }
 
     public void mostrarCategoria() {
